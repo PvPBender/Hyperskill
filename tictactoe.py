ec0c55dc6dccc9ebe3ff0130c9b831eb6c2580e7
PvPBender <me@pvpbender.com>
2023-05-14 19:47:02 +0200

    Simple Tic-Tac-Toe (Python), Stage 5/5

--- tictactoe.py
+++ tictactoe.py
@@ -1,37 +1,86 @@
-def tic_tac_input(input_):
-    input_ = input_.replace("_", " ")
-    game_board = [[' '] * 3 for _ in range(3)]  # creates the 2D game board with empty cells
-    for i in range(3):
-        game_board[i] = list(input_[i * 3: (i + 1) * 3])
-
-    return game_board
-
-
-def tic_tac_print():
+def game_print():
     print(f"""
-          ---------\n
-          | {' '.join(grid[0])} |\n
-          | {' '.join(grid[1])} |\n
-          | {' '.join(grid[2])} |\n
-          ---------\n
-          """)
+    ---------
+    | {' '.join(grid[0])} |
+    | {' '.join(grid[1])} |
+    | {' '.join(grid[2])} |
+    ---------
+    """)
 
 
-grid = tic_tac_input(input())
-
-tic_tac_print()
-
-while True:
-    try:
-        coordinates = [int(x) - 1 for x in input().split()]
-    except ValueError:
-        print("You should enter numbers!")
-    else:
-        if not (0 <= coordinates[0] <= 2 and 0 <= coordinates[1] <= 2):
-            print("Coordinates should be from 1 to 3!")
-        elif grid[coordinates[0]][coordinates[1]] != " ":
-            print("This cell is occupied! Choose another one!")
+def enter_move(side):
+    while True:
+        input_ = input()
+        if len(input_) != 3:
+            print("The input must be in the format of Y X")
         else:
-            grid[coordinates[0]][coordinates[1]] = "X"
-            tic_tac_print()
-            break
+            try:
+                coordinates = [int(x) - 1 for x in input_.split()]
+            except ValueError:
+                print("You should enter numbers!")
+            else:
+                if not (0 <= coordinates[0] <= 2 and 0 <= coordinates[1] <= 2):
+                    print("Coordinates should be from 1 to 3!")
+                elif grid[coordinates[0]][coordinates[1]] != " ":
+                    print("This cell is occupied! Choose another one!")
+                else:
+                    grid[coordinates[0]][coordinates[1]] = side
+                    game_print()
+                    return grid, coordinates
+
+
+def check_row(player) -> bool:
+    return all([True if cell == player else False for cell in grid[coordinates[0]]])
+
+
+def check_column(player) -> bool:
+    for i in range(3):
+        if grid[i][coordinates[0]] != player:
+            return False
+    else:
+        return True
+
+
+def check_diagonals(player) -> bool:
+    if grid[1][1] == player:
+        if grid[0][0] == player and grid[2][2] == player:
+            return True
+        elif grid[0][2] == player and grid[2][0] == player:
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
+def check_win(player) -> bool:
+    if check_row(player):
+        print(f"{player} wins")
+        return True
+    elif check_column(player):
+        print(f"{player} wins")
+        return True
+    elif check_diagonals(player):
+        print(f"{player} wins")
+        return True
+    else:
+        print(grid[coordinates[0]])
+        print([True if cell == player else False for cell in grid[coordinates[0]]])
+        return False
+
+
+grid = [[' '] * 3 for _ in range(3)]  # creates the 2D game board with empty cells
+
+game_print()
+
+# GAME: 3 * 3 moves
+for i in range(9):
+    if i % 2 == 0:
+        side = "X"
+    else:
+        side = "O"
+    grid, coordinates = enter_move(side)
+    if check_win(side):
+        break
+else:
+    print("Draw")
