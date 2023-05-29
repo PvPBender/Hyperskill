bc9ed4e32e56198f187f93cd062da92177b0bf9c
PvPBender <me@pvpbender.com>
2023-05-29 22:38:59 +0200

    Hangman (Python), Stage 4/8

--- Introduction to Python/Hangman (Python)/hangman.py
+++ Introduction to Python/Hangman (Python)/hangman.py
@@ -1,9 +1,22 @@
 from random import choice
 
-words = ("python", "java", "swift", "javascript")
 
-print("H A N G M A N")
-if input("Guess the word: ") == choice(words):
-    print("You survived!")
-else:
-    print("You lost!")
+def hintify(to_guess):
+    to_guess_hinted = to_guess[:3] + "-" * max(0, (len(to_guess) - 3))
+    return to_guess_hinted
+
+
+def main():
+    words = ("python", "java", "swift", "javascript")
+
+    to_guess = choice(words)
+
+    print("H A N G M A N")
+    if input(f"Guess the word {hintify(to_guess)}: ") == to_guess:
+        print("You survived!")
+    else:
+        print("You lost!")
+
+
+if __name__ == "__main__":
+    main()
