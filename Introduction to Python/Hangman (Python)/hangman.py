d239fc217512f9fbc3f31e8de93bee39c39cf9df
PvPBender <me@pvpbender.com>
2023-05-30 17:07:13 +0200

    Hangman (Python), Stage 5/8

--- Introduction to Python/Hangman (Python)/hangman.py
+++ Introduction to Python/Hangman (Python)/hangman.py
@@ -1,21 +1,22 @@
 from random import choice
 
-
-def hintify(to_guess):
-    to_guess_hinted = to_guess[:3] + "-" * max(0, (len(to_guess) - 3))
-    return to_guess_hinted
-
-
 def main():
-    words = ("python", "java", "swift", "javascript")
-
-    to_guess = choice(words)
-
     print("H A N G M A N")
-    if input(f"Guess the word {hintify(to_guess)}: ") == to_guess:
-        print("You survived!")
-    else:
-        print("You lost!")
+
+    to_guess = choice(("python", "java", "swift", "javascript"))
+    to_guess_list = ['-' for _ in to_guess]
+    to_guess_set = set(to_guess)
+
+    attempts = 8
+    for i in range(attempts):
+        print('\n' + ''.join(to_guess_list))
+
+        if (guessed := input("Input a letter: ")) in to_guess_set:
+            to_guess_list = [guessed if x == guessed else to_guess_list[j] for j, x in enumerate(to_guess)]
+        else:
+            print("That letter doesn't appear in the word.")
+
+    print("Thanks for playing!")
 
 
 if __name__ == "__main__":
