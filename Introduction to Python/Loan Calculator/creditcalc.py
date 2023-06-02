575ee3849fe43a54734e5ee31c373fe129fa7269
PvPBender <me@pvpbender.com>
2023-06-02 22:42:34 +0200

    Loan Calculator, Stage 3/4

--- Introduction to Python/Loan Calculator/creditcalc.py
+++ Introduction to Python/Loan Calculator/creditcalc.py
@@ -1,49 +1,65 @@
-from math import ceil
+from math import ceil, log
+from textwrap import dedent
 
-enter_principal = "Enter the loan principal:"
-question = """
+
+def get_n_of_months() -> int:
+    principal = float(input("Enter the loan principal:\n"))
+    monthly_payment = float(input("Enter the monthly payment:\n"))
+    monthly_interest = float(input("Enter the loan interest:\n")) / 12 / 100
+
+    return int(ceil(log(monthly_payment / (monthly_payment - monthly_interest * principal), 1 + monthly_interest)))
+
+
+def get_monthly_payment() -> int:
+    principal = float(input("Enter the loan principal:\n"))
+    n_of_months = int(input("Enter the number of periods:\n"))
+    monthly_interest = float(input("Enter the loan interest:\n")) / 12 / 100
+
+    interest_compound_factor = monthly_interest * ((1 + monthly_interest) ** n_of_months)
+    interest_growth_factor = ((1 + monthly_interest) ** n_of_months) - 1
+    payment_per_dollar = interest_compound_factor / interest_growth_factor
+
+    return ceil(principal * payment_per_dollar)
+
+
+def get_principal() -> int:
+    monthly_payment = float(input("Enter the monthly payment:\n"))
+    n_of_months = int(input("Enter the number of periods:\n"))
+    monthly_interest = float(input("Enter the loan interest:\n")) / 12 / 100
+
+    interest_compound_factor = monthly_interest * ((1 + monthly_interest) ** n_of_months)
+    interest_growth_factor = ((1 + monthly_interest) ** n_of_months) - 1
+    payment_per_dollar = interest_compound_factor / interest_growth_factor
+
+    return int(monthly_payment / payment_per_dollar)
+
+
+def main():
+    question = dedent("""
     What do you want to calculate?
-    type "m" - for number of monthly payments,
-    type "p" - for the monthly payment:
-    """
-enter_payment = "Enter the monthly payment:"
-enter_months = "Enter the number of months:"
+    type "n" for number of monthly payments,
+    type "a" for annuity monthly payment amount,
+    type "p" for loan principal:
+    """)
 
+    if (to_calculate := input(question)) == "n":
+        n_of_months = get_n_of_months()
+        n_of_years = n_of_months // 12
+        n_of_months %= 12
+        years_str = f"{n_of_years} year{'s' if n_of_years > 1 else ''} " if n_of_years else ""
+        and_str = "and " if n_of_months and n_of_years else ""
+        months_str = f"{n_of_months} month{'s' if n_of_months != 1 else ''} " if n_of_months or not years_str else ""
+        print(f"It will take {years_str}{and_str}{months_str}to repay the loan!")
 
-def ask_principal():
-    print(enter_principal)
-    return float(input())
+    elif to_calculate == "a":
+        print(f"Your monthly payment = {get_monthly_payment()}!")
 
+    elif to_calculate == "p":
+        print(f"Your loan principal = {get_principal()}!")
 
-def ask_which():
-    print(question)
-    return input()
-
-
-def get_months():
-    print(enter_payment)
-    payments = float(input())
-    return principal / payments
-
-
-def get_payment():
-    print(enter_months)
-    months = int(input())
-    return ceil(principal / months), int(principal % ceil(principal / months))
-
-
-principal = ask_principal()
-if ask_which() == "m":
-    months = get_months()
-    if months == 1:
-        print("It will take 1 month to repay the loan")
     else:
-        print(f"It will take {ceil(months)} months to repay the loan")
-else:
-    months = get_payment()
-    payments = months[0]
-    last_payment = months[1]
-    if last_payment != 0:
-        print(f"Your monthly payment = {payments} and the last payment = {last_payment}")
-    else:
-        print(f"Your monthly payment = {payments}")
+        print('You can only type "n", "a", or "p"')
+
+
+if __name__ == "__main__":
+    main()
