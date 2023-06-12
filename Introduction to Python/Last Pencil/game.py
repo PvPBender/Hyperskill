b4532a3dfe9e52bd17b06931e68c73bf7d04a148
PvPBender <me@pvpbender.com>
2023-06-12 19:42:30 +0200

    Last Pencil, Stage 5/5

--- Introduction to Python/Last Pencil/game.py
+++ Introduction to Python/Last Pencil/game.py
@@ -1,12 +1,9 @@
-import string  # I would just use isdigit(), but I have to use the string module
-
-
 def get_pencil_num():
     print("How many pencils would you like to use:")
     while True:
         pencil_number = input()
 
-        if all(char in string.digits for char in pencil_number):
+        if pencil_number.isdigit():
             pencil_number = int(pencil_number)
         else:
             print("The number of pencils should be numeric")
@@ -19,19 +16,19 @@ def get_pencil_num():
         return pencil_number
 
 
-def select_name(name_1="John", name_2="Jack"):
-    selected_name = input(f"Who will be the first ({name_1}, {name_2}):\n")
+def select_name(player="John", bot="Jack"):
+    selected_name = input(f"Who will be the first ({player}, {bot}):\n")
 
-    if selected_name == name_1:
-        return name_1, name_2
-    elif selected_name == name_2:
-        return name_2, name_1
-    elif selected_name not in (name_1, name_2):
-        print(f"Choose between {name_1} and {name_2}")
-        return select_name(name_1, name_2)
+    if selected_name == player:
+        return player, bot, True
+    elif selected_name == bot:
+        return bot, player, False
+    elif selected_name not in (player, bot):
+        print(f"Choose between {player} and {bot}")
+        return select_name(player, bot)
 
 
-def get_taken_pencils(pencil_num):
+def player_takes_pencil(pencil_num):
     while True:
         try:
             taken_pencils = int(input())
@@ -44,28 +41,50 @@ def get_taken_pencils(pencil_num):
         if taken_pencils > pencil_num:
             print("Too many pencils were taken")
             continue
+
         return taken_pencils
+
+
+def bot_takes_pencil(pencil_num):
+    match pencil_num % 4:
+        case 0:
+            print(3)
+            return 3  # winning
+        case 1:
+            print(1)
+            return 1  # losing
+        case 2:
+            print(1)
+            return 1  # winning
+        case 3:
+            print(2)
+            return 2  # winning
 
 
 def main():
     pencil_num = get_pencil_num()
+    player_1, player_2, player_first = select_name()
 
-    name_1, name_2 = select_name()
-
-    for i in range(pencil_num):
+    for i in range(pencil_num + 1):
         if pencil_num == 0:
             if i % 2 == 0:
-                print(f"{name_1} won")
+                print(f"{player_1} won")
             else:
-                print(f"{name_2} won")
+                print(f"{player_2} won")
             break
         print("|" * pencil_num)
         if i % 2 == 0:
-            print(f"{name_1}'s turn:\n")
+            print(f"{player_1}'s turn:\n")
+            if player_first:
+                pencil_num -= player_takes_pencil(pencil_num)
+            else:
+                pencil_num -= bot_takes_pencil(pencil_num)
         else:
-            print(f"{name_2}'s turn:\n")
-
-        pencil_num -= get_taken_pencils(pencil_num)
+            print(f"{player_2}'s turn:\n")
+            if player_first:
+                pencil_num -= bot_takes_pencil(pencil_num)
+            else:
+                pencil_num -= player_takes_pencil(pencil_num)
 
 
 if __name__ == "__main__":
